307b3d64316989c426881fce283e7d1cc9d5d716
Saad Ahmed <sadisfaction570@gmail.com>
2024-07-25 20:46:12 +0500

    update

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -76,7 +76,7 @@ function Home2() {
               </li> */}
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/saadahmedrana/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
